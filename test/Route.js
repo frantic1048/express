20b2a4a21d219663ffe2c7b1c47acb6753fae7c1
LeviG <tianjianxiao@foxmail.com>
2016-01-19 23:32:19 +0800

    Levi: update Route.js req.accepts.js

--- test/Route.js
+++ test/Route.js
@@ -1,4 +1,5 @@
 
+//导入模块
 var after = require('after');
 var should = require('should');
 var express = require('../')
@@ -6,18 +7,24 @@ var express = require('../')
   , methods = require('methods')
   , assert = require('assert');
 
+//Route测试模块
 describe('Route', function(){
 
+  //.all测试模块
   describe('.all', function(){
+    //检测
     it('should add handler', function(done){
       var req = { method: 'GET', url: '/' };
       var route = new Route('/foo');
 
+      //得到http请求便将called设置为true
       route.all(function(req, res, next) {
         req.called = true;
         next();
       });
 
+      //将get请求路由给route下所有路径
+      //若called值设置正确，用例通过
       route.dispatch(req, {}, function (err) {
         if (err) return done(err);
         should(req.called).be.ok;
@@ -25,30 +32,36 @@ describe('Route', function(){
       });
     })
 
+    //检测能否响应所有http请求
     it('should handle VERBS', function(done) {
       var count = 0;
       var route = new Route('/foo');
+      //接受所有方法后通过用例
       var cb = after(methods.length, function (err) {
         if (err) return done(err);
         count.should.equal(methods.length);
         done();
       });
 
+      //对所有请求计数
       route.all(function(req, res, next) {
         count++;
         next();
       });
 
+      //将所有的方法分发给route下所有路径
       methods.forEach(function testMethod(method) {
         var req = { method: method, url: '/' };
         route.dispatch(req, {}, cb);
       });
     })
 
+    //检测是否堆叠调用
     it('should stack', function(done) {
       var req = { count: 0, method: 'GET', url: '/' };
       var route = new Route('/foo');
 
+      //每个对route的请求都将经过两个响应
       route.all(function(req, res, next) {
         req.count++;
         next();
@@ -59,6 +72,7 @@ describe('Route', function(){
         next();
       });
 
+      //若成功得到两次响应，用例通过
       route.dispatch(req, {}, function (err) {
         if (err) return done(err);
         req.count.should.equal(2);
@@ -67,16 +81,20 @@ describe('Route', function(){
     })
   })
 
+  //.VERB测试模块
   describe('.VERB', function(){
+    //检测是否支持get方法
     it('should support .get', function(done){
       var req = { method: 'GET', url: '/' };
       var route = new Route('');
 
+      //对get方法设置响应
       route.get(function(req, res, next) {
         req.called = true;
         next();
       })
 
+      //若get方法能得到预期结果，用例通过
       route.dispatch(req, {}, function (err) {
         if (err) return done(err);
         should(req.called).be.ok;
@@ -84,10 +102,12 @@ describe('Route', function(){
       });
     })
 
+    //检测是否只对对应请求响应
     it('should limit to just .VERB', function(done){
       var req = { method: 'POST', url: '/' };
       var route = new Route('');
 
+      //若响应了get会报错
       route.get(function(req, res, next) {
         throw new Error('not me!');
       })
@@ -97,6 +117,7 @@ describe('Route', function(){
         next();
       })
 
+      //若只响应了post，用例通过
       route.dispatch(req, {}, function (err) {
         if (err) return done(err);
         should(req.called).be.true;
@@ -104,10 +125,12 @@ describe('Route', function(){
       });
     })
 
+    //检测是否允许连续调用
     it('should allow fallthrough', function(done){
       var req = { order: '', method: 'GET', url: '/' };
       var route = new Route('');
 
+      //依次给空字符串添加'a','b','c'
       route.get(function(req, res, next) {
         req.order += 'a';
         next();
@@ -123,6 +146,7 @@ describe('Route', function(){
         next();
       })
 
+      //若得到'abc'返回值则用例通过
       route.dispatch(req, {}, function (err) {
         if (err) return done(err);
         req.order.should.equal('abc');
@@ -131,15 +155,19 @@ describe('Route', function(){
     })
   })
 
+  //错误检测模块
   describe('errors', function(){
+    //检测是否通过四参数的函数处理错误
     it('should handle errors via arity 4 functions', function(done){
       var req = { order: '', method: 'GET', url: '/' };
       var route = new Route('');
 
+      //报'foobar'错
       route.all(function(req, res, next){
         next(new Error('foobar'));
       });
 
+      //若从三参数函数经过，添加'0',否则,添加'a'
       route.all(function(req, res, next){
         req.order += '0';
         next();
@@ -150,6 +178,7 @@ describe('Route', function(){
         next(err);
       });
 
+      //若req.order的值为'a'，则用例通过
       route.dispatch(req, {}, function (err) {
         should(err).be.ok;
         should(err.message).equal('foobar');
@@ -158,10 +187,12 @@ describe('Route', function(){
       });
     })
 
+    //检测没有next()是否能处理错误
     it('should handle throw', function(done) {
       var req = { order: '', method: 'GET', url: '/' };
       var route = new Route('');
 
+      //仅仅抛出错误而没有将其传递
       route.all(function(req, res, next){
         throw new Error('foobar');
       });
@@ -176,6 +207,7 @@ describe('Route', function(){
         next(err);
       });
 
+      //若req.order的值仍为'a'，则用例通过
       route.dispatch(req, {}, function (err) {
         should(err).be.ok;
         should(err.message).equal('foobar');
@@ -184,23 +216,28 @@ describe('Route', function(){
       });
     });
 
+    //检测能否处理内部抛出的 错误处理
     it('should handle throwing inside error handlers', function(done) {
       var req = { method: 'GET', url: '/' };
       var route = new Route('');
 
+      //报'bppm!'错
       route.get(function(req, res, next){
         throw new Error('boom!');
       });
 
+      //处理错误，再报'oops'错
       route.get(function(err, req, res, next){
         throw new Error('oops');
       });
 
+      //处理错误处理报出的'oops'错
       route.get(function(err, req, res, next){
         req.message = err.message;
         next();
       });
 
+      //若处理错误处理的处理成功运行，用例通过
       route.dispatch(req, {}, function (err) {
         if (err) return done(err);
         should(req.message).equal('oops');
@@ -208,14 +245,17 @@ describe('Route', function(){
       });
     });
 
+    //检测.all中抛出的错误有没有被处理
     it('should handle throw in .all', function(done) {
       var req = { method: 'GET', url: '/' };
       var route = new Route('');
 
+      //.all中抛出了错误
       route.all(function(req, res, next){
         throw new Error('boom!');
       });
 
+      //若返回正确的错误，用例通过
       route.dispatch(req, {}, function(err){
         should(err).be.ok;
         err.message.should.equal('boom!');
@@ -223,15 +263,17 @@ describe('Route', function(){
       });
     });
 
+    //检测只有错误处理的能否正常运行
     it('should handle single error handler', function(done) {
       var req = { method: 'GET', url: '/' };
       var route = new Route('');
 
+      //该部分将不会运行
       route.all(function(err, req, res, next){
-        // this should not execute
         true.should.be.false;
       });
 
+      //若正常运行（不经过错误处理），用例通过
       route.dispatch(req, {}, done);
     });
   })
--- test/req.accepts.js
+++ test/req.accepts.js
@@ -1,21 +1,28 @@
 
+//导入模块
 var express = require('../')
   , request = require('supertest');
 
+//req测试模块
 describe('req', function(){
+  //.accepts(type)测试模块
   describe('.accepts(type)', function(){
+    //检测未设置accept时是否返回true
     it('should return true when Accept is not present', function(done){
       var app = express();
 
+      //收到json返回true，否则false
       app.use(function(req, res, next){
         res.end(req.accepts('json') ? 'yes' : 'no');
       });
 
+      //不设置accept
       request(app)
       .get('/')
       .expect('yes', done);
     })
 
+    //检测设置相同accept时是否返回true
     it('should return true when present', function(done){
       var app = express();
 
@@ -23,12 +30,14 @@ describe('req', function(){
         res.end(req.accepts('json') ? 'yes' : 'no');
       });
 
+      //设置accept为相同的json
       request(app)
       .get('/')
       .set('Accept', 'application/json')
       .expect('yes', done);
     })
 
+    //检测设置不同accept时是否返回false
     it('should return false otherwise', function(done){
       var app = express();
 
@@ -36,6 +45,7 @@ describe('req', function(){
         res.end(req.accepts('json') ? 'yes' : 'no');
       });
 
+      //设置accept为相同的html
       request(app)
       .get('/')
       .set('Accept', 'text/html')
@@ -43,9 +53,11 @@ describe('req', function(){
     })
   })
 
+  //检测是否接受一个列表的类型
   it('should accept an argument list of type names', function(done){
     var app = express();
 
+    //设置多个接受类型
     app.use(function(req, res, next){
       res.end(req.accepts('json', 'html'));
     });
@@ -56,45 +68,57 @@ describe('req', function(){
     .expect('json', done);
   })
 
+  //.accepts(types)测试模块
   describe('.accepts(types)', function(){
+    //检测当accept未设置时是否返回第一个类型
     it('should return the first when Accept is not present', function(done){
       var app = express();
 
+      //设置多个类型，json为第一个
       app.use(function(req, res, next){
         res.end(req.accepts(['json', 'html']));
       });
 
+      //若返回json，用例通过
       request(app)
       .get('/')
       .expect('json', done);
     })
 
+    //检测当accept设置时是否返回第一个匹配的类型
     it('should return the first acceptable type', function(done){
       var app = express();
 
+      //设置多个类型，json为第一个
+      //accept设置的参数为html类型，应该返回第二个
       app.use(function(req, res, next){
         res.end(req.accepts(['json', 'html']));
       });
 
+      //若返回html，用例通过
       request(app)
       .get('/')
       .set('Accept', 'text/html')
       .expect('html', done);
     })
 
+    //检测当没有类型匹配时是否返回false
     it('should return false when no match is made', function(done){
       var app = express();
 
+      //设置不同于accept参数的类型
       app.use(function(req, res, next){
         res.end(req.accepts(['text/html', 'application/json']) ? 'yup' : 'nope');
       });
 
+      //若返回false，用例通过
       request(app)
       .get('/')
       .set('Accept', 'foo/bar, bar/baz')
       .expect('nope', done);
     })
 
+    //检测能否获得accept中的quality参数
     it('should take quality into account', function(done){
       var app = express();
 
@@ -102,12 +126,14 @@ describe('req', function(){
         res.end(req.accepts(['text/html', 'application/json']));
       });
 
+      //设置quality参数，若成功返回提取参数后的类型，用例通过
       request(app)
       .get('/')
       .set('Accept', '*/html; q=.5, application/json')
       .expect('application/json', done);
     })
 
+    //检测设置模糊类型时能否返回第一个匹配的类型
     it('should return the first acceptable type with canonical mime types', function(done){
       var app = express();
 
@@ -115,6 +141,8 @@ describe('req', function(){
         res.end(req.accepts(['application/json', 'text/html']));
       });
 
+      //设置模糊类型'*/html'
+      //若返回第一个匹配的类型'text/html'，用例通过
       request(app)
       .get('/')
       .set('Accept', '*/html')
